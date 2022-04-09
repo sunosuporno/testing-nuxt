6cb93fc57c87bd2ba2dfa79bb1bdddfa5e1f82aa
sunosuporno <sarkarsuporno36@gmail.com>
2022-04-09 12:56:00 +0530

    cha-ching

---
+++ pages/hello.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1 class="text-sky-700">This is hello page</h1>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h1 class="text-2xl text-blue-500 m-5">This is the index page</h1>
+    <NuxtLink to="/hello" class="m-6 text-yellow-600">Go to Hello Page</NuxtLink>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
